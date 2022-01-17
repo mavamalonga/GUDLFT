dc987dcb3b8a460faa4f4272759828da0a9928e4
Alpha <mavamalonga.alpha@gmail.com>
2022-01-17 14:45:21 +0100

    add TestShowSummary

--- tests.py
+++ tests.py
@@ -2,37 +2,57 @@ import unittest
 from server import app, loadClubs, loadCompetitions
 
 
-class TestDataFunctions(unittest.TestCase):
+class TestDataJson(unittest.TestCase):
 
-	def test_loadClubs(self):
-		first_club = {
+	def test_loadClubs_function(self):
+		club = {
 			"name":"Simply Lift",
 			"email":"john@simplylift.co",
 			"points":"13"
 			}
-		self.assertEqual(first_club, loadClubs()[0])
+		self.assertEqual(club, loadClubs()[0])
 
-	def test_loadCompetitions(self):
-		first_comp = {
+	def test_loadCompetitions_function(self):
+		competition = {
 			"name": "Spring Festival",
 			"date": "2020-03-27 10:00:00",
 			"numberOfPlaces": "25"
 		}
-		self.assertEqual(first_comp, loadCompetitions()[0])
+		self.assertEqual(competition, loadCompetitions()[0])
 
 
-class TestIndexFunction(unittest.TestCase):
+class TestIndex(unittest.TestCase):
 
 	client = app.test_client()
 	response = client.get('/')
 
-	def test_index_status(self):
+	def test_status(self):
 		statuscode = self.response.status_code
 		self.assertEqual(statuscode, 200)
 
-	def test_index_html_content(self):
+	def test_html_content(self):
 		b = b'Welcome to the GUDLFT Registration Portal!'
 		self.assertIn(b, self.response.data)
+
+
+class TestShowSummary(unittest.TestCase):
+
+	client = app.test_client()
+	club = loadClubs()[0]
+	response = client.post('/showSummary', data={'email': club['email']})
+
+	def test_status(self):
+		statuscode = self.response.status_code
+		self.assertEqual(statuscode, 200)
+
+	def test_html_content(self):
+		content = f"Welcome, {self.club['email']}"
+		self.assertIn(content, str(self.response.data))
+
+
+
+
+
 
 
 
