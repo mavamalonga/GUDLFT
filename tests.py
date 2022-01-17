bc4b699a98412ebc8af9e2c2b2b6e4c38f05cae2
Alpha <mavamalonga.alpha@gmail.com>
2022-01-17 15:20:34 +0100

    add testBook

--- tests.py
+++ tests.py
@@ -50,8 +50,21 @@ class TestShowSummary(unittest.TestCase):
 		self.assertIn(content, str(self.response.data))
 
 
+class TestBook(unittest.TestCase):
 
+	client = app.test_client()
+	club = loadClubs()[0]
+	competition = loadCompetitions()[0]
+	endpoint = f"/book/{competition['name']}/{club['name']}"
+	response = client.get(endpoint)
 
+	def test_status(self):
+		statuscode = self.response.status_code
+		self.assertEqual(statuscode, 200)
+	
+	def test_html_content(self):
+		self.assertIn(b'Booking for Spring Festival', self.response.data)
+		self.assertIn(b'Places available: 25', self.response.data)
 
 
 
