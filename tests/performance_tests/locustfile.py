f6f837bec833d126fb8dfe81062f1936c93c41f1
Alpha <mavamalonga.alpha@gmail.com>
2022-01-20 16:51:54 +0100

    purchase places

--- tests/performance_tests/locustfile.py
+++ tests/performance_tests/locustfile.py
@@ -2,6 +2,12 @@ from locust import HttpUser, task
 import os
 import sys
 
+path = 'C:\\Users\\HP\\Desktop\\GUDLFT\\'
+os.chdir(path)
+sys.path.insert(1, path)
+
+from server import app, loadClubs, loadCompetitions
+
 class ProjectPerfTest(HttpUser):
 	@task
 	def index(self):
@@ -14,3 +20,26 @@ class ProjectPerfTest(HttpUser):
 	@task 
 	def show_summary(self):
 		self.client.post('showSummary', data={'email': 'john@simplylift.co'})
+
+	@task
+	def book(self):
+		club = loadClubs()[0]
+		competition = loadCompetitions()[0]
+		endpoint = f"book/{competition['name']}/{club['name']}"
+		self.client.get(endpoint)
+
+	@task 
+	def clubPoints(self):
+		club = loadClubs()[1]
+		competition = loadCompetitions()[0]
+		data = {
+			"club": club['name'],
+			"competition": competition['name'],
+			"places": 10
+		}
+		self.client.post('purchasePlaces', data=data)
+
+	
+
+
+
